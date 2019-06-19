2c7d947eff7f6b100bf8c07a7333992bd1692a91
vshimoda <shimoda@3-shake.com>
2019-06-19 19:31:42 +0900

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # homebrew-aws-ssh
-AWS SSH via Session Manager
+
+Login to EC2 using Session Manager
+
+# Installation
+
+```
+brew tap 3-shake/aws-ssh
+
+brew install aws-ssh
+```
+
+
+# Usage
+
+```
+$ aws-ssh
+
+# select your instance
+QUERY> 
+i-xxxxxxxxxxxxxxxxx instance-1
+i-yyyyyyyyyyyyyyyyy instance-2
+i-zzzzzzzzzzzzzzzzz instance-3
+
+# session start
+Starting session with SessionId: 3-shake-xxxxxxxxxxxxxxxxx
+
+sh-4.2$
+```
+
---
+++ aws-ssh.rb
@@ -0,0 +1,27 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class AwsSsh < Formula
+  desc "AWS SSH using Session Manager"
+  homepage "https://github.com/3-shake/homebrew-aws-ssh"
+  url "https://github.com/3-shake/homebrew-aws-ssh.git",
+    :tag      => "v0.1"
+  depends_on "peco"
+
+  def install
+    bin.install "bin/aws-ssh"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test homebrew-aws-ssh`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
